e6a16efd8843412483adbde3589d7d5031011711
Bas Steins <sebastian.steins@gmail.com>
2025-11-02 22:36:30 +0100

    feat: Add `apt-get` wrapper

---
+++ internal/installers/apt_get/apt_get.go
@@ -0,0 +1,41 @@
+package aptget
+
+func InstallPackage(pkg string) error {
+	// Implementation for installing a package using apt-get
+	return nil
+}
+
+func CleanUp() error {
+	// Implementation for cleaning up apt-get caches
+	return nil
+}
+
+func AddPpaRepository(ppa string) error {
+	// Implementation for adding a PPA repository
+	return nil
+}
+
+func AddAptRepository(repo string) error {
+	// Implementation for adding a custom apt repository
+	return nil
+}
+
+func UpdatePackageLists() error {
+	// Implementation for updating apt package lists
+	return nil
+}
+
+func RunUpgrade() error {
+	// Implementation for running apt-get upgrade
+	return nil
+}
+
+func RemovePackage(pkg string) error {
+	// Implementation for removing a package using apt-get
+	return nil
+}
+
+func IsPackageInstalled(pkg string) (bool, error) {
+	// Implementation for checking if a package is installed
+	return false, nil
+}
